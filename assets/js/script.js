c5c5697bf9ad31d4a92f03b5230f379f6003bdb2
Roman Grubic <grubic.roman@gmail.com>
2020-01-15 10:18:27 +0000

    New pictures, match logic done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,32 @@
+var turnedCard = false;
+var firstCard, secondCard;
+
+
 // Function flipCard
 
-$(".memory-card").click(function(){
+$(".memory-card").click(function () {
     this.classList.toggle("flip");
-    console.log(this);
+    if (!turnedCard) {
+        turnedCard = true;
+        firstCard = this;
+    } else {
+        turnedCard = false;
+        secondCard = this;
+
+        matchCard();
+    }
 });
+
+function matchCard() {
+    if (firstCard.dataset.image === secondCard.dataset.image) {
+        console.log("Do they match? yes")
+
+    } else {
+        console.log("Do they match? no")
+        setTimeout(function () {
+            firstCard.classList.toggle("flip")
+            secondCard.classList.toggle("flip")
+        }, 1000)
+    }
+};
+
