56d8a07148ea1f0524b7357b19d9a69f4eafabca
Roman Grubic <grubic.roman@gmail.com>
2020-01-16 12:56:29 +0000

    Added flip counter

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,24 +4,25 @@ var firstCard, secondCard;
 var stopGame = false; 
 var library = document.querySelectorAll(".card");
 
-// --- Time
-var time = 5; // make options for 20 / 30 / 40 seconds
-
 // --- Shuffle on loading the game page
 window.onload = function(){
+    flipAdd();
     library.forEach(card => {   
         var randomNumber = Math.floor(Math.random() * (library.length));     
         card.style.order = randomNumber;
+        
 })
 };
 
 // --- Shuffle the library via the reset button and reset Timer
 $("#resetBoard").click(function() {
+    setTimer = time;
+    document.getElementById("flip-counter").innerHTML = flip;
+    stopGame = false;
     library.forEach(card => {   
         var randomNumber = Math.floor(Math.random() * (library.length));     
         card.style.order = randomNumber;
-        flipBack() 
-        setTimer = time;
+        flipBack();      
     })
 });
 
@@ -49,11 +50,13 @@ function matchCard() {
     if (firstCard.dataset.image === secondCard.dataset.image) {
         console.log("Do they match? Yes! Have a cookie");
         setTimer += 5;
+        flipAdd();
 
     } else {
         console.log("Do they match? No! Wait for next turn");
         setTimer -= 1;
         stopGame = true;
+        flipAdd();
         setTimeout(function () {
             firstCard.classList.toggle("flip");
             secondCard.classList.toggle("flip");
@@ -62,14 +65,30 @@ function matchCard() {
     }
 }; 
 
+
+// ---Start Time
+var time = 20; // make options for 20 / 30 / 40 seconds
+
 // --- Timer 
 var gameTimer = document.getElementById("game-timer");
 var setTimer = time;
 var timer = setInterval(function(){    
-  gameTimer.innerHTML = "Time left: "+ setTimer + "sec";
+  gameTimer.innerHTML = setTimer + "sec";
   setTimer -= 1;
   if(setTimer <= 0){
-    gameTimer.innerHTML = "Game over! Board is locked! Reset to try again!";
+    gameTimer.innerHTML = "- Game over! Board is locked! Reset to try again! -";
     stopGame = true;
   }
 }, 1000);
+
+// --- Flip counter
+var flip = 0;
+var flipCounter = document.getElementById("flip-counter");
+var flipTotal = flip;
+
+
+function flipAdd(){
+    flipCounter.innerHTML = flipTotal + " flips";
+    flipTotal += 1;
+}
+
