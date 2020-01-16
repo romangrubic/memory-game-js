e0b29f1d0ee46d79a79a6be1f72d7b88e35ac61f
Roman Grubic <grubic.roman@gmail.com>
2020-01-16 14:09:05 +0000

    Added matched cards counter

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,36 @@
 // Card variables
 var turnedCard = false;
 var firstCard, secondCard;
-var stopGame = false; 
+var stopGame = false;
 var library = document.querySelectorAll(".card");
 
+// ---Start Time
+var time = 20; // make options for 20 / 30 / 40 seconds
+
 // --- Shuffle on loading the game page
-window.onload = function(){
+window.onload = function () {
     flipAdd();
-    library.forEach(card => {   
-        var randomNumber = Math.floor(Math.random() * (library.length));     
-        card.style.order = randomNumber;
-        
-})
+    matchAdd();
+    randomOrder();
 };
 
-// --- Shuffle the library via the reset button and reset Timer
-$("#resetBoard").click(function() {
+// --- Reset button
+$("#resetBoard").click(function () {
     setTimer = time;
-    document.getElementById("flip-counter").innerHTML = flip;
+    resetFlip();
+    matchedReset();
+    flipBack();
+    randomOrder();
     stopGame = false;
-    library.forEach(card => {   
-        var randomNumber = Math.floor(Math.random() * (library.length));     
-        card.style.order = randomNumber;
-        flipBack();      
-    })
+    
 });
+// --- Random order for cards
+function randomOrder() {
+    library.forEach(card => {
+        var randomNumber = Math.floor(Math.random() * (library.length));
+        card.style.order = randomNumber;
+    });
+};
 
 // --- Flip card
 $(".card").click(function flipCard() {
@@ -41,7 +47,7 @@ $(".card").click(function flipCard() {
 });
 
 // --- Flip it back
-function flipBack(){
+function flipBack() {
     $("div").filter(".flip").removeClass("flip");
 }
 
@@ -51,6 +57,7 @@ function matchCard() {
         console.log("Do they match? Yes! Have a cookie");
         setTimer += 5;
         flipAdd();
+        matchAdd()
 
     } else {
         console.log("Do they match? No! Wait for next turn");
@@ -63,32 +70,54 @@ function matchCard() {
             stopGame = false;
         }, 1500)
     }
-}; 
-
-
-// ---Start Time
-var time = 20; // make options for 20 / 30 / 40 seconds
+};
 
 // --- Timer 
 var gameTimer = document.getElementById("game-timer");
 var setTimer = time;
-var timer = setInterval(function(){    
-  gameTimer.innerHTML = setTimer + "sec";
-  setTimer -= 1;
-  if(setTimer <= 0){
-    gameTimer.innerHTML = "- Game over! Board is locked! Reset to try again! -";
-    stopGame = true;
-  }
+var timer = setInterval(function () {
+    gameTimer.innerHTML = setTimer + "sec";
+    setTimer -= 1;
+    if (setTimer <= 0) {
+        gameTimer.innerHTML = "- Game over! Board is locked! Reset to try again! -";
+        stopGame = true;
+    }
 }, 1000);
 
-// --- Flip counter
-var flip = 0;
-var flipCounter = document.getElementById("flip-counter");
-var flipTotal = flip;
-
-
-function flipAdd(){
-    flipCounter.innerHTML = flipTotal + " flips";
-    flipTotal += 1;
+function setTimer(){
+    setTimer = time;
 }
+
+// --- Flip counter
+var flipCounter = document.getElementById("flip-counter");
+var flip = 0;
+
+function resetFlip(){
+    if (flip > 0) { flip = 1 };
+    document.getElementById("flip-counter").innerHTML = 0;
+}
+
+function flipAdd() {
+    flipCounter.innerHTML = flip;
+    flip += 1;
+}
+
+// --- Matched counter
+var matchCounter = document.getElementById("matched-counter")
+var match = 0
+
+function matchedReset(){
+    if (match > 0) { match = 1 };
+    document.getElementById("matched-counter").innerHTML = 0;
+}
+
+function matchAdd() {
+    matchCounter.innerHTML = match;
+    match += 1;
+}
+
+// Game finish - all matched
+function stopGame {
+//    if (matchedCards = 8) stop the time
+//}
 
